f190f0c63df196214e2375ae5140f0d0ef622ac6
dkbrz <glaz.dikobraz@gmail.com>
2020-04-06 09:10:10 +0300

    geo convertion

--- notebooks/geocodes.py
+++ notebooks/geocodes.py
@@ -1,16 +1,29 @@
 import pandas as pd
-
-df = pd.read_csv('geocodes.csv', sep='\t')
+import mysql.connector
+db = mysql.connector.connect(user='root', password='password', database='folklore_db2', charset='utf8mb4')
+cur = db.cursor()
+df = pd.read_sql_query("""
+    SELECT 
+        id,
+        map_region as region,
+        map_district as district,
+        village_name as village,
+        map_lat as lat,
+        map_lon as lon,
+        years
+    FROM g_villages
+    WHERE map_lat IS NOT NULL 
+    """, con=db)
 
 pattern = """
 <table>
     <tr>
         <td>Область</td>
-        <td>Х</td>
+        <td>{}</td>
     </tr>
     <tr>
         <td>Район</td>
-        <td>Х</td>
+        <td>{}</td>
     </tr>
     <tr>
         <td>Населенный пункт</td>
@@ -21,24 +34,42 @@ pattern = """
         <td>{}</td>
     </tr>
     <tr>
-        <td><a href='http://wikipedia.org'>Искать в корпусе</a></td>
+        <td><a target="_blank" href='https://linghub.ru/folklore/results?&submit=Поиск&village={}'>Искать в корпусе</a></td>
     </tr>
     <tr>
-        <td><img src='https://github.com/dkbrz/folklore/blob/dev/folklore_app/static/img/search_in_progress.gif?raw=true'></td>
+        <td><img src='https://raw.githubusercontent.com/dkbrz/folklore_images/master/map_images/try.png'></td>
     </tr>
 </table>
 """
 
-df['Номер'] = list(range(len(df)))
-df['Год обследования'] = df['Год обследования'].apply(lambda x: ';'.join([i for i in str(x).split('.') if i != '0']))
-df = df[['Широта', 'Долгота', 'Год обследования', 'Название населенного пункта', 'Номер']]
+# df['Номер'] = list(range(len(df)))
+# df['Год обследования'] = df['Год обследования'].apply(lambda x: ';'.join([i for i in str(x).split('.') if i != '0']))
+# df = df[['Широта', 'Долгота', 'Год обследования', 'Название населенного пункта', 'Номер']]
+# data = []
+# for i in range(df.shape[0]):
+#     row = df.iloc[i]
+#     data.append(pattern.format(
+#         row['Название населенного пункта'],
+#         row['Год обследования']
+#     ))
+# df['Год обследования'] = data
+# # print(df)
 data = []
 for i in range(df.shape[0]):
     row = df.iloc[i]
-    data.append(pattern.format(
-        row['Название населенного пункта'],
-        row['Год обследования']
+    data.append((
+        row['lat'],
+        row['lon'],
+        pattern.format(
+            row['region'],
+            row['district'],
+            row['village'],
+            row['years'],
+            row['village']
+        ),
+        row['village'],
+        row['id']
     ))
-df['Год обследования'] = data
-# print(df)
-df.to_csv('changed.csv', sep='\t', index=False)
+data = pd.DataFrame(data)
+data.columns = ['Широта', 'Долгота', 'Год обследования', 'Название населенного пункта', 'Номер']
+data.to_csv('changed2.csv', sep='\t', index=False)
