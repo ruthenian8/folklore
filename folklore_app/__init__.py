848c7c700f745017ee82b866ec735170ba2c122c
dkbrz <glaz.dikobraz@gmail.com>
2020-07-07 20:40:17 +0300

    admin inside

--- folklore_app/__init__.py
+++ folklore_app/__init__.py
@@ -145,6 +145,50 @@ def create_app():
     app.secret_key = 'yyjzqy9ffY'
     db.app = app
     db.init_app(app)
+
+    # -------- Admin ----------
+
+    class AdminIndexView(f_admin.AdminIndexView):
+        @expose('/')
+        def index(self):
+            # print('1', current_user)
+            # print(current_user.is_authenticated)
+            if not current_user.is_authenticated:
+                return redirect(url_for("login"))
+            # import pdb;pdb.set_trace()
+            # if current_user. == True:
+            #     return super(AdminIndexView, self).index()
+            # else:
+            return super(AdminIndexView, self).index()
+
+    admin = Admin(
+        app, name='Folklore Admin',
+        template_mode='bootstrap3',
+        index_view=AdminIndexView()
+    )
+    # admin.add_link(MenuLink(name='Архив', url='/'))
+
+    admin.add_view(FolkloreBaseView(Texts, db.session, name='Тексты'))
+
+    admin.add_view(CreateOnly(User, db.session, category="Люди", name='Пользователи'))
+    admin.add_view(FolkloreBaseView(Collectors, db.session, category="Люди", name='Собиратели'))
+    admin.add_view(FolkloreBaseView(Informators, db.session, category="Люди", name='Информанты'))
+
+    admin.add_view(EditOnly(Keywords, db.session, category="Жанры, слова", name='Ключевые слова'))
+    admin.add_view(EditOnly(Genres, db.session, category="Жанры, слова", name='Жанры'))
+
+    admin.add_view(EditOnly(Questions, db.session, category="Опросники", name='Вопросы'))
+    admin.add_view(EditOnly(QListName, db.session, category="Опросники", name='Опросники'))
+
+    admin.add_view(FolkloreBaseView(GeoText, db.session, category="География", name='Географический объект'))
+    admin.add_view(NoDeleteView(Region, db.session, category="География", name='Регион'))
+    admin.add_view(NoDeleteView(District, db.session, category="География", name='Район'))
+    admin.add_view(NoDeleteView(Village, db.session, category="География", name='Населенный пункт'))
+
+    admin.add_view(ViewOnly(GImages, db.session, category="Галерея", name='Изображения'))
+    admin.add_view(EditOnly(GTags, db.session, category="Галерея", name='Теги'))
+
+    # -------------------------
     return app
 
 
@@ -163,52 +207,6 @@ app.config.update(dict(
     LANGUAGES=settings['interface_languages'],
     BABEL_DEFAULT_LOCALE='ru'
 ))
-
-# -------- Admin ----------
-
-
-class AdminIndexView(f_admin.AdminIndexView):
-    @expose('/')
-    def index(self):
-        # print('1', current_user)
-        # print(current_user.is_authenticated)
-        if not current_user.is_authenticated:
-            return redirect(url_for("login"))
-        # import pdb;pdb.set_trace()
-        # if current_user. == True:
-        #     return super(AdminIndexView, self).index()
-        # else:
-        return super(AdminIndexView, self).index()
-
-
-admin = Admin(
-    app, name='Folklore Admin',
-    template_mode='bootstrap3',
-    index_view=AdminIndexView()
-)
-# admin.add_link(MenuLink(name='Архив', url='/'))
-
-admin.add_view(FolkloreBaseView(Texts, db.session, name='Тексты'))
-
-admin.add_view(CreateOnly(User, db.session, category="Люди", name='Пользователи'))
-admin.add_view(FolkloreBaseView(Collectors, db.session, category="Люди", name='Собиратели'))
-admin.add_view(FolkloreBaseView(Informators, db.session, category="Люди", name='Информанты'))
-
-admin.add_view(EditOnly(Keywords, db.session, category="Жанры, слова", name='Ключевые слова'))
-admin.add_view(EditOnly(Genres, db.session, category="Жанры, слова", name='Жанры'))
-
-admin.add_view(EditOnly(Questions, db.session, category="Опросники", name='Вопросы'))
-admin.add_view(EditOnly(QListName, db.session, category="Опросники", name='Опросники'))
-
-admin.add_view(FolkloreBaseView(GeoText, db.session, category="География", name='Географический объект'))
-admin.add_view(NoDeleteView(Region, db.session, category="География", name='Регион'))
-admin.add_view(NoDeleteView(District, db.session, category="География", name='Район'))
-admin.add_view(NoDeleteView(Village, db.session, category="География", name='Населенный пункт'))
-
-admin.add_view(ViewOnly(GImages, db.session, category="Галерея", name='Изображения'))
-admin.add_view(EditOnly(GTags, db.session, category="Галерея", name='Теги'))
-
-# -------------------------
 
 
 @app.context_processor
